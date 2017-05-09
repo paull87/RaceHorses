aace2be88f86558a0060071cc9dc8105ad50a078
Paul Lucas <paullucas87@gmail.com>
2017-05-09 08:59:18 +0100

    Refactored twitter_dm.py
    Refactored twitter_dm.py for readability

--- twitter_dm.py
+++ twitter_dm.py
@@ -3,27 +3,51 @@
 from DownloadHorses import Horses
 from twitter_api import TwitterAPI
 
-twitter = TwitterAPI()
 
-todays_horses = Horses('http://www.horseracebase.com/atoz.php')
+def get_horses(searches):
+    """Finds today's horses."""
+    todays_horses = Horses('http://www.horseracebase.com/atoz.php')
+    return todays_horses.search_horses(searches)
 
-key_words = [
-    'Poppy',
-    'Pop',
-    'October',
-    'Gem',
-    'Gemma',
-    'Lucas',
-    'June',
-    'Spring Vale',
-    'Reverdy',
-    'Garner',
-]
 
-my_horses = todays_horses.search_horses(key_words)
-message = "These are today's horses -\n\n" + my_horses
+def send_dm(handle, text):
+    """Sends the given messaage as a dm to the given user."""
+    twitter = TwitterAPI()
+    try:
+        twitter.api.send_direct_message(user=handle, text=text)
+    except Exception as e:
+        raise 'Unable to send dm: {}'.format(e)
 
-try:
-    twitter.api.send_direct_message(user='plucas87', text=message)
-except Exception as e:
-    raise 'Unable to send dm: {}'.format(e)
+
+def create_message(items):
+    """creates message depending on content of given text"""
+    if items != '':
+        return "These are today's horses -\n\n" + items
+    else:
+        return "There are no horses running today that meet your searches."
+
+
+if __name__ == '__main__':
+
+    # User to send the dm to
+    user = 'plucas87'
+
+    # Key words to search horse names for
+    key_words = [
+        'Poppy',
+        'Pop',
+        'October',
+        'Gem',
+        'Gemma',
+        'Lucas',
+        'June',
+        'Spring Vale',
+        'Reverdy',
+        'Garner',
+    ]
+
+    # Find today's horses
+    horses = get_horses(key_words)
+    
+    # Send the dm message
+    send_dm(user, create_message(horses))
