729a52bd25605295b6daa823a0dd75ec4b654129
Paul Lucas <paullucas87@gmail.com>
2017-05-12 14:14:21 +0100

    Update twitter_api.py
    Changed so that keys and tokens are pulled from a separate json file.

--- twitterbot/twitter_api.py
+++ twitterbot/twitter_api.py
@@ -1,25 +1,52 @@
 #!/usr/bin/python3
 
+
 import tweepy
+import json
+
 
 class TwitterAPI:
     """Class for the authentication of twitter api."""
 
     def __init__(self):
         """Initialise the twitter api."""
-        self.api = authenticate_twitter()
+        self.api = self.authenticate_twitter()
+        self.keys = get_keys()
+
+    def key(self, key_name):
+        """Returns the key value for the given key name."""
+        try:
+            return self.keys[key_name]
+        except KeyError as e:
+            raise 'No key found for {}'.format(key_name)
+
+    def authenticate_twitter(self):
+        """Authenticates the twitter API from the given tokens."""
+        consumer_key = self.key['consumer_key']
+        consumer_secret = self.key['consumer_secret']
+        access_key = self.key['access_key']
+        access_secret = self.key['access_secret']
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_key, access_secret)
+        try:
+            return tweepy.API(auth)
+        except Exception as e:
+            raise 'Unable to authenticate twitter API: {}'.format(e)
 
 
-def authenticate_twitter():
-    """Authenticates the twitter API from the given tokens."""
-    consumer_key = ''
-    consumer_secret = ''
-    access_key = ''
-    access_secret = ''
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
+def get_keys():
+    """Gets the keys from the json file."""
     try:
-        return tweepy.API(auth)
+        with open('keys.json') as key_file:
+            return json.loads(key_file.read())
     except Exception as e:
-        raise 'Unable to authenticate twitter API: {}'.format(e)
+        raise 'Unable to open json file: {}'.format(e)
+
+
+def key(key_name, keys=keys):
+    """Returns the key value for the given key name."""
+    try:
+        return keys[key_name]
+    except KeyError as e:
+        raise 'No key found for {}'.format(key_name)
 
